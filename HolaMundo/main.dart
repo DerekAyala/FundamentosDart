37c67b7071c1a5c88a3c445525e49970f19e4540
DerekAyala <derek.ayala12345@hotmail.com>
2022-11-09 09:49:24 -0600

    Hola mundo comentarios comentarios de funciones

---
+++ HolaMundo/main.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  // Hola mundo
+  print("hola mundo");
+  // Hola mundi sin salto de linea
+  stdout.write("Hola Mundo\n");
+  // suma
+  print(2+4);
+  print(suma(2, 4));
+}
+
+
+/// Funcion para sumar dos numeros enteros
+/// recibe dos numeros enteros
+/// suma(2,4)
+/// 6
+int suma(int n1,int n2) {
+  return n1 + n2;
+}
