dee626a0f313260add1a5584dd8db0acdc3b0580
DerekAyala <derek.ayala12345@hotmail.com>
2022-11-10 08:23:55 -0600

    Variables y tipos de datos

---
+++ Variables y Tipos de datos/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  var numero; // tipado por inferencia
+  print(numero);
+  int numero2 = 10; // tipado explicito
+  print(numero2);
+  int entero = 0;
+  double flotantes = 0.0;
+  num numerica= 0.0;
+  String texto = "cero";
+  bool booleanos = true;
+  dynamic dinamico = "dinamico";
+}
